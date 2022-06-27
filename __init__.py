26b8699907d449708d976673f544f7ea19243843
minnellf <filippo.minnella@polito.it>
2022-06-27 10:56:59 +0200

    changed config files

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from .quant_accumulator import ClampQuantAccumulator, TruncQuantAccumulator
+from .quant_activation import QuantReLU, QuantSigmoid, QuantTanh, QuantHardTanh, QuantIdentity
+from .quant_avg_pool import QuantAvgPool2d, QuantAdaptiveAvgPool2d
+from .quant_linear import QuantLinear
+from .quant_bn import BatchNorm1dToQuantScaleBias, BatchNorm2dToQuantScaleBias
+from .quant_scale_bias import ScaleBias, QuantScaleBias
+from .hadamard_classifier import HadamardClassifier
+from .quant_convtranspose import QuantConvTranspose1d, QuantConvTranspose2d
+from .quant_conv import QuantConv1d, QuantConv2d
+from .quant_add import QuantAdd2d
+from .quant_eltwise import QuantEltwiseAdd, QuantCat
+from .quant_max_pool import QuantMaxPool1d, QuantMaxPool2d
+from .quant_upsample import QuantUpsample, QuantUpsamplingBilinear2d, QuantUpsamplingNearest2d
+from .quant_dropout import QuantDropout
